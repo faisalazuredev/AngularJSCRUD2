e8685ee0bdfd4407df0ed2ffe589bb4ae8b1555a
Dummytester644 <60654783+Dummytester644@users.noreply.github.com>
2020-12-21 17:04:50 +0500

    Reverted the fix on insert query at insertEmployees.php file.

--- databaseFiles/insertDetails.php
+++ databaseFiles/insertDetails.php
@@ -2,7 +2,7 @@
 // Including database connections
 require_once 'database_connections.php';
 // Fetching and decoding the inserted data
-//
+
 $data = json_decode(file_get_contents("php://input")); 
 // Escaping special characters from submitting data & storing in new variables.
 $name = mysqli_real_escape_string($con, $data->name);
@@ -14,8 +14,8 @@ $address = mysqli_real_escape_string($con, $data->address);
 $status = '1';
 
 // mysqli insert query
-$query = "INSERT into emp_details (emp_name,emp_email,emp_gender,emp_address, status) 
-                           VALUES ('$name','$email','$gender','$address', '$status')";
+$query = "INSERT into emp_details (emp_name,emp_email,emp_gender,emp_address) 
+                           VALUES ('$name','$email','$gender','$address')";
 // Inserting data into database
 mysqli_query($con, $query);
 echo true;
